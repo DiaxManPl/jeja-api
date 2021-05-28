9763a8ff490be6bbece380da1df167681c58ab80
DiaxManPl <diaxman.gameplay@gmail.com>
2021-05-28 08:43:20 +0200

    Wrapper ready (without README)

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 const axios = require("axios")
 const cheerio = require("cheerio")
 const config = require("./config.json")
+
+const randomMeme = async () => {
+    const $ = cheerio.load((await axios.get(config.url.memes)).data)
+    const element = $(".ob-left-image.ob-image-j").first()
+    const src = element.attr("src")
+    const name = element.attr("alt")
+   return {
+       image: src,
+       name,
+   }
+}
+
+const randomJoke = async () => {
+    const $ = cheerio.load((await axios.get(config.url.jokes)).data)
+    const element = $("div.dow-left-text").first()
+    const text = element.children("p").first().text()
+    return text
+}
+
+module.exports = {
+    randomMeme,
+    randomJoke
+}
